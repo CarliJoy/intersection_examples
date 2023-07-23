6c7a8ac53c917babf7a14f3f1cdae506f127b4ce
Carli* Freudenberg <carli.freudenberg@energymeteo.de>
2023-07-23 20:14:10 +0200

    add two abc example

---
+++ two_abc.py
@@ -0,0 +1,44 @@
+from collections.abc import Container, Iterable, Iterator
+from typing_extensions import reveal_type
+
+from basedtyping import Intersection
+
+from typing import TypeVar
+
+T = TypeVar("T")
+
+class IterableContainer(Iterable[int], Container[int]):
+    ...
+
+
+def f(x: IterableContainer) -> None:
+    ...
+
+
+def g(x: Intersection[Iterable[int], Container[int]]) -> None:
+    if 1 not in x:
+        # Debug output
+        for it in x:
+            print(it)
+            reveal_type(it)
+
+
+class Test(IterableContainer[int]):
+    def __iter__(self) -> Iterator[int]:
+        yield 1
+
+    def __contains__(self, item: int) -> bool:
+        return True
+
+
+
+def assertIn(item: T, thing: Intersection[Iterable[T], Container[T]]) -> None:
+    if item not in thing:
+        # Debug output
+        for it in thing:
+            print(it)
+            reveal_type(it)
+
+f(Test())  # Fails
+g(Test())  # Okay
+assertIn(1, [1,2]) # Okay
