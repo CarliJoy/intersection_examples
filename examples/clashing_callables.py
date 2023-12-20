6f903493d120dd80309554596453f1adcaf56638
mark-todd <markpeter.todd@hotmail.co.uk>
2023-12-20 16:54:34 +0000

    Made repo pass type checks

--- examples/clashing_callables.py
+++ examples/clashing_callables.py
@@ -3,4 +3,3 @@ from typing import Callable
 from intersection_examples import Intersection
 
 test = Intersection[Callable[[int], int], Callable[[int], str]]
-print(test)
--- examples/overloaded_already.py
+++ examples/overloaded_already.py
@@ -6,27 +6,27 @@ from intersection_examples import Intersection
 class A:
     @overload
     def foo(self, x: "A") -> "A":
-        return 1
+        ...
 
     @overload
     def foo(self, x: type) -> type:
-        pass
+        ...
 
     def foo(self, x: "type | A") -> "type | A":
-        pass
+        ...
 
 
 class B:
     @overload
     def foo(self, x: int) -> int:
-        return 1
+        ...
 
     @overload
     def foo(self, x: str) -> str:
-        pass
+        ...
 
     def foo(self, x: int | str) -> int | str:
-        pass
+        ...
 
 
 test = Intersection[A, B]
